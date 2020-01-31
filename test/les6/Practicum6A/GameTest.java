6f3b29a436ccd8d98427e0fcb676dc9ff49e843d
andre.donk <andre.donk@hu.nl>
2020-01-31 17:32:16 +0100

    missing import statement

--- test/les6/Practicum6A/GameTest.java
+++ test/les6/Practicum6A/GameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
     private Game game1JrOud;
